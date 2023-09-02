4a1a25e8e4d9bb8002c19191e43395d30fca7690
shafikul-islam <179398ssshofik@gmail.com>
2023-09-02 09:08:59 +0600

    [DONE]role based all category route

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -1,11 +1,25 @@
 import express from 'express';
 import { CategoryController } from './category.controller';
+import Auth from '../../middlewares/Auth';
+import { ENUM_USER_ROLE } from '../../../enum/user';
 const router = express.Router();
 //TODO:Role based routing system
 //create-category route,update route,delete route are protected by ADMIN
-router.post('/create-category', CategoryController.create);
+router.post(
+  '/create-category',
+  Auth(ENUM_USER_ROLE.ADMIN),
+  CategoryController.create
+);
 router.get('/', CategoryController.getAllCategory);
 router.get('/:id', CategoryController.getSingleCategory);
-router.patch('/:id', CategoryController.updateCategory);
-router.delete('/:id', CategoryController.deleteCategory);
+router.patch(
+  '/:id',
+  Auth(ENUM_USER_ROLE.ADMIN),
+  CategoryController.updateCategory
+);
+router.delete(
+  '/:id',
+  Auth(ENUM_USER_ROLE.ADMIN),
+  CategoryController.deleteCategory
+);
 export const CategoryRoutes = router;
