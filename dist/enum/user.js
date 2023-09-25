823b41ee6c308fd24e7f293e3d94231082af4e7e
shafikul-islam <179398ssshofik@gmail.com>
2023-09-25 17:47:43 +0600

    final check

--- dist/enum/user.js
+++ dist/enum/user.js
@@ -6,4 +6,4 @@ var ENUM_USER_ROLE;
 (function (ENUM_USER_ROLE) {
     ENUM_USER_ROLE["ADMIN"] = "admin";
     ENUM_USER_ROLE["CUSTOMER"] = "customer";
-})(ENUM_USER_ROLE = exports.ENUM_USER_ROLE || (exports.ENUM_USER_ROLE = {}));
+})(ENUM_USER_ROLE || (exports.ENUM_USER_ROLE = ENUM_USER_ROLE = {}));
